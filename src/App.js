77d1c0204d031195410cdf2af7e72719675b77ba
parvin-aziza <60470807+parvin-aziza@users.noreply.github.com>
2022-09-06 13:33:17 +0530

    Color picker project (#1)
    * Initialize project using Create React App
    
    * Added React color picker component
    
    * added comments and modified ui

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { ReactColorPicker } from './ReactColorPicker';
+
+function App() {
+  return (
+    <div className="App">
+    <ReactColorPicker />
+    </div>
+  );
+}
+
+export default App;
